3f3be3ee9cec6b957a717cc8607420d5472c2900
kiclu <kiclu@protonmail.com>
2023-06-07 01:13:58 +0200

    code refactor and reorganize

---
+++ h/map.h
@@ -0,0 +1,50 @@
+#ifndef _ARP_MAP_H
+#define _ARP_MAP_H
+
+#include<stdint.h>
+
+#define background_color 0x001
+
+void clear_screen();
+void draw_map();
+
+#define map_line_count 265
+#define map_line_color 0x11C
+
+typedef struct{
+    uint32_t x0;
+    uint32_t y0;
+    uint32_t x1;
+    uint32_t y1;
+} line_t;
+
+void draw_map_lines();
+void draw_door();
+
+#define map_rect_count 19
+#define map_rect_color 0x11C
+
+typedef struct{
+    uint32_t x0;
+    uint32_t y0;
+    uint32_t x1;
+    uint32_t y1;
+} rect_t;
+
+void draw_map_rects();
+
+#define map_dot_count 681
+#define map_dot_color 0xFFF
+
+typedef struct{
+    uint32_t x;
+    uint32_t y;
+} dot_t;
+
+uint32_t check_dot_eaten(uint32_t idx);
+void set_dot_eaten(uint32_t idx);
+void clear_dot_eaten();
+
+void draw_map_dots();
+
+#endif//_ARP_MAP_H
--- h/pm.h
+++ h/pm.h
@@ -1,6 +1,30 @@
 #ifndef _ARP_PM_H
 #define _ARP_PM_H
 
+#include<stdint.h>
+
+#define pacman_color 0xFF0
+
+typedef struct {
+    uint32_t pos_x;
+    uint32_t pos_y;
+    uint32_t dir;
+    uint32_t newdir;
+    uint32_t fr;
+    uint32_t stuck;
+} pacman_t;
+
+typedef struct {
+    uint32_t pos_x;
+    uint32_t pos_y;
+    uint32_t dir;
+    uint32_t newdir;
+    uint32_t fr;
+    uint32_t color;
+    uint32_t stuck;
+    uint32_t is_released;
+} ghost_t;
+
 void run();
 
 #endif//_ARP_PM_H
